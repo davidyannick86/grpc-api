0d061733aa74d9ccc7fa1b43de911647504903ea
DavidYannick <57354908+davidyannick86@users.noreply.github.com>
2025-04-10 04:45:54 +0200

    fix: Update .gitignore to include additional directories and ensure consistent formatting; refactor mongo_connect.go for timeout handling

--- internals/repositories/mongodb/mongo_connect.go
+++ internals/repositories/mongodb/mongo_connect.go
@@ -2,6 +2,7 @@ package mongodb
 
 import (
 	"context"
+	"time"
 
 	"github.com/davidyannick86/grpc-api-mongodb/pkg/utils"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -10,6 +11,8 @@ import (
 
 func CreateMongoClient() (*mongo.Client, error) {
 	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
 
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
