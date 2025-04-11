03042d361a1064206644728e51f812a54db02bfd
DavidYannick <57354908+davidyannick86@users.noreply.github.com>
2025-04-11 09:12:06 +0200

    fix: Refactor teacher filtering and sorting logic in GetTeachers function

--- internals/api/handlers/teachers.go
+++ internals/api/handlers/teachers.go
@@ -2,9 +2,14 @@ package handlers
 
 import (
 	"context"
+	"log"
+	"reflect"
+	"strings"
 
+	"github.com/davidyannick86/grpc-api-mongodb/internals/models"
 	"github.com/davidyannick86/grpc-api-mongodb/internals/repositories/mongodb"
 	pb "github.com/davidyannick86/grpc-api-mongodb/proto/gen"
+	"go.mongodb.org/mongo-driver/bson"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -23,4 +28,71 @@ func (s *Server) AddTeachers(ctx context.Context, req *pb.Teachers) (*pb.Teacher
 	}
 
 	return &pb.Teachers{Teachers: addedTeachers}, nil
+}
+
+func (s *Server) GetTeachers(ctx context.Context, req *pb.GetTeachersRequest) (*pb.Teachers, error) {
+	// filtering
+	buildFilterForTeacher(req)
+
+	// sorting
+	sortOptions := buildSortOptions(req.GetSortBy())
+	log.Println("sortOptions", sortOptions)
+
+	// access data
+
+	return nil, nil
+}
+
+func buildFilterForTeacher(req *pb.GetTeachersRequest) {
+	filter := bson.M{} // It's a map,  M is an unordered representation of a BSON document
+
+	var modelTeacher models.Teacher
+
+	modelVal := reflect.ValueOf(&modelTeacher).Elem()
+	modelType := modelVal.Type()
+
+	reqVal := reflect.ValueOf(req.Teacher).Elem()
+	reqType := reqVal.Type()
+
+	for i := range reqVal.NumField() {
+		fieldVal := reqVal.Field(i)
+		fieldName := reqType.Field(i).Name
+
+		if fieldVal.IsValid() && !fieldVal.IsZero() {
+			modelField := modelVal.FieldByName(fieldName)
+			if modelField.IsValid() && modelField.CanSet() {
+				modelField.Set(fieldVal)
+			}
+		}
+	}
+
+	// iterate over the modelTeacher to build filter using bson.M
+	for i := range modelVal.NumField() {
+		fieldVal := modelVal.Field(i)
+		//fieldName := modelType.Field(i).Name
+
+		if fieldVal.IsValid() && !fieldVal.IsZero() {
+			bsonTag := modelType.Field(i).Tag.Get("bson")
+			bsonTag = strings.TrimSuffix(bsonTag, ",omitempty")
+
+			filter[bsonTag] = fieldVal.Interface().(string)
+		}
+	}
+
+	log.Println("filter", filter)
+
+}
+
+func buildSortOptions(sortFields []*pb.SortField) bson.D {
+	var sortOptions bson.D
+
+	for _, sortField := range sortFields {
+		order := 1
+		if sortField.GetOrder() == pb.Order_DESC {
+			order = -1
+		}
+		sortOptions = append(sortOptions, bson.E{Key: sortField.Field, Value: order})
+	}
+
+	return sortOptions
 }
